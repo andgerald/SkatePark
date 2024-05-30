2832d6da50709eac3922d47a4d4ebb77750197f1
andgeral <geraldinebecerra93@gmail.com>
2024-05-30 18:57:06 -0400

    se agrega ruta para actualizar los datos de skate

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 
 const router = Router();
 
-//ruta get para handlebars
+//ruta get para  vistas de handlebars
 router.get("/", skatersController.home);
 router.get("/admin", skatersController.admin);
 router.get("/registro", skatersController.registro);
@@ -13,6 +13,8 @@ router.get("/login", skatersController.login);
 router.get("/skaters", skatersController.findAll);
 //ruta delete para eliminar un skaters por su id
 router.delete("/skaters/:id", skatersController.remove);
+//ruta para actulizar datos de skaterk
+router.put("/skaters", skatersController.update);
 //ruta para actulizar el estado
 router.put("/skaters/status/:id", skatersController.updateState);
 
