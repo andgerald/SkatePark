f1102d29c549b127f8c5dc80f4a8eb8934e17d04
andgeral <geraldinebecerra93@gmail.com>
2024-05-30 17:12:58 -0400

    se agrega controllers para eliminar y para la vista admin

--- controllers/index.js
+++ controllers/index.js
@@ -12,6 +12,22 @@ const findAll = async (req, res) => {
   }
 };
 
+const remove = async (req, res) => {
+  const { id } = req.params;
+  if (!id) {
+    res.status(400).send("EL id es requerido");
+  }
+  try {
+    const skaters = await skatersModel.remove(id);
+    res.status(200).send({ message: "Skater Eliminado con éxito", skaters });
+  } catch (error) {
+    res.status(500).send({
+      error: `Algo salió mal... ${e}`,
+      code: 500,
+    });
+  }
+};
+
 const home = async (req, res) => {
   try {
     const skaters = await skatersModel.findAll();
@@ -24,7 +40,21 @@ const home = async (req, res) => {
   }
 };
 
+const admin = async (req, res) => {
+  try {
+    const skaters = await skatersModel.findAll();
+    res.render("Admin", { skaters });
+  } catch (e) {
+    res.status(500).send({
+      error: `Algo salió mal... ${e}`,
+      code: 500,
+    });
+  }
+};
+
 export const skatersController = {
   findAll,
+  remove,
   home,
+  admin,
 };
